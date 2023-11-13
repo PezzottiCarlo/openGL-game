a77a5e663467dcb20f160167a91dcb45222f5503
Jari Naeser <jari.naeser@student.supsi.ch>
2023-11-13 15:56:35 +0100

    add: mesh class

---
+++ engine/mesh.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file    mesh.cpp
+ * @brief   A simple mesh class
+ *
+ * @author  Jari Näser (C) SUPSI [jari.naeser@student.supsi.ch]
+ * @author  Carlo pezzotti (C) SUPSI [carlo.pezzotti@student.supsi.ch]
+ */
+
+
+ //////////////
+ // #INCLUDE //
+ //////////////
+
+ // Library main include:
+#include "mesh.h"
+#include "engine.h"
+
+ ///////////////////////
+// BODY OF CLASS Mesh //
+////////////////////////
+
+// TBD
---
+++ engine/mesh.h
@@ -0,0 +1,29 @@
+/**
+ * @file	node.h
+ * @brief	A simple node class
+ *
+ * @author	Jari Näser (C) SUPSI [jari.naeser@student.supsi.ch]
+ * @author  Carlo pezzotti (C) SUPSI [carlo.pezzotti@student.supsi.ch]
+ */
+
+#pragma once
+
+
+ //////////////
+ // #INCLUDE //
+ //////////////
+#include "node.h"
+#include "engine.h"
+
+
+/////////////
+// CLASSES //
+/////////////
+
+/**
+ * @brief Simple mesh class.
+ */
+class LIB_API Mesh : public Node {
+
+};
+
